5845193f5ccc675fcd33b1b34872ec81560f3169
Your Name <118735992@.con>
2019-03-18 17:44:54 +0800

    封装局部插件完成

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <!-- <jishuqi :initnum="20" :dfmax="30" :dfmin="0" :bushu="3"></jishuqi> -->
+    <jishuqi :initnum="20" :dfmax="30" :dfmin="0" :bushu="3"></jishuqi>
   </div>
 </template>
 
